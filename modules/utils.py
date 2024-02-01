5ca57c50614b9f2e6666048092a9ec255918f8a2
Xavier919 <xavier.lapointe@usherbrooke.ca>
2024-02-01 02:56:53 -0500

    removed fake news text headers

--- modules/utils.py
+++ modules/utils.py
@@ -1,9 +1,11 @@
 import pandas as pd
 
+
 def build_dataset(true_path, false_path, num_class_samples=-1):
     true_df = pd.read_csv(true_path)
     true_df['is_real'] = 1
     false_df = pd.read_csv(false_path)
     false_df['is_real'] = 0
+    false_df['text'] = false_df['text'].str.split('-').apply(lambda x: ' '.join(x[1:]))
     df = pd.concat([true_df[:num_class_samples], false_df[:num_class_samples]], ignore_index=True)
     return df.T.to_dict()
