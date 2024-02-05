03eb8e6440e6ad49440603b49438a560427d9fa7
Xavier919 <xavier.lapointe@usherbrooke.ca>
2024-02-05 15:13:48 -0500

    modif data parser

--- modules/utils.py
+++ modules/utils.py
@@ -3,22 +3,36 @@ import pandas as pd
 mapping = {'politicsNews': 0, 'worldnews': 1, 'News': 2, 'politics': 3, 'left-news': 4, 
                'Government News': 5, 'US_News': 6, 'Middle-east': 7}
 
-def build_dataset(true_path, false_path, num_class_samples=-1):
+def build_dataset(true_path, false_path, num_class_samples=-1, rnd_state=42):
     true_df = pd.read_csv(true_path)
     true_df['is_real'] = 1
+    
     false_df = pd.read_csv(false_path)
     false_df['is_real'] = 0
     false_df['text'] = false_df['text'].str.split('-').apply(lambda x: ' '.join(x[1:]))
+    
+    true_df = true_df[(true_df['text'].str.strip() != "") & (true_df['title'].str.strip() != "")]
+    false_df = false_df[(false_df['text'].str.strip() != "") & (false_df['title'].str.strip() != "")]
 
     if num_class_samples == -1:
         true_sampled_df = true_df
         false_sampled_df = false_df
     else:
-        true_sampled_df = true_df.sample(n=min(len(true_df), num_class_samples), replace=False, random_state=42)
-        false_sampled_df = false_df.sample(n=min(len(false_df), num_class_samples), replace=False, random_state=42)
+        true_sampled_df = true_df.sample(n=min(len(true_df), num_class_samples), replace=False, random_state=rnd_state)
+        false_sampled_df = false_df.sample(n=min(len(false_df), num_class_samples), replace=False, random_state=rnd_state)
 
     df = pd.concat([true_sampled_df, false_sampled_df], ignore_index=True)
 
     df['subject'] = df['subject'].map(mapping)
+
+    return df.T.to_dict()
+
+
+def build_dataset(path, num_class_samples=-1, rnd_state=42):
+    df = pd.read_excel(path, engine='openpyxl')
+    if num_class_samples == -1:
+        df = df
+    else:
+        df = df.sample(n=min(len(df), num_class_samples), replace=False, random_state=rnd_state)
 
     return df.T.to_dict()
