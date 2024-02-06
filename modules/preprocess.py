6d9edaca316e309f594386a8e8a5ea4955356b6a
Xavier919 <xavier.lapointe@usherbrooke.ca>
2024-02-06 12:39:03 -0500

    up preprocess

--- modules/preprocess.py
+++ modules/preprocess.py
@@ -1,10 +1,26 @@
 import spacy
+import re
+import string
 
 
-def spacy_tokenizer(text, lowercase=False):
+def spacy_tokenizer(text):
     nlp = spacy.load("en_core_web_sm")
-    if lowercase:
-        return [tok.text.lower() for tok in nlp.tokenizer(str(text))]
-    else:
-        return [tok.text for tok in nlp.tokenizer(str(text))]
-    
+    return [tok.text for tok in nlp.tokenizer(str(text))]
+
+
+
+def dataset_modif(dataset):
+    #convert all numbers to num
+    for attrs in dataset.values():
+        attrs['tweet'] = re.sub(r'\d+', 'num', attrs['tweet'])
+    #convert everything beginning with @... to @
+    for attrs in dataset.values():
+        attrs['tweet'] = re.sub(r'@\w+', '@', attrs['tweet'])
+    #remove anything with \n in it
+    for attrs in dataset.values():
+        attrs['tweet'] = re.sub(r'\n', '', attrs['tweet'])
+    #remove all punctuation
+    for attrs in dataset.values():
+        pattern = f"[{re.escape(string.punctuation)}]"
+        attrs['tweet'] = re.sub(pattern, '', attrs['tweet'])
+    return dataset
