df5c30a13668c41b710281165a28ddca708e4391
Xavier919 <xavier.lapointe@usherbrooke.ca>
2024-02-07 12:17:02 -0500

    up modules

--- modules/preprocess.py
+++ modules/preprocess.py
@@ -1,26 +1,42 @@
 import spacy
 import re
 import string
+import nltk
+from nltk.corpus import stopwords
 
+nltk.download('stopwords')
 
 def spacy_tokenizer(text):
     nlp = spacy.load("en_core_web_sm")
     return [tok.text for tok in nlp.tokenizer(str(text))]
 
+def text_edit(dataset,num=False,mention=False,newline=False,punctuation=False,stop_words_=False):
+    stop_words = set(stopwords.words('english')) #define set of english stop words
+    
+    for attrs in dataset.values():
+        tweet = attrs['tweet']
 
+        #all numbers to 'num'
+        if num:
+            tweet = re.sub(r'\d+', 'num', tweet)  
 
-def dataset_modif(dataset):
-    #convert all numbers to num
-    for attrs in dataset.values():
-        attrs['tweet'] = re.sub(r'\d+', 'num', attrs['tweet'])
-    #convert everything beginning with @... to @
-    for attrs in dataset.values():
-        attrs['tweet'] = re.sub(r'@\w+', '@', attrs['tweet'])
-    #remove anything with \n in it
-    for attrs in dataset.values():
-        attrs['tweet'] = re.sub(r'\n', '', attrs['tweet'])
-    #remove all punctuation
-    for attrs in dataset.values():
-        pattern = f"[{re.escape(string.punctuation)}]"
-        attrs['tweet'] = re.sub(pattern, '', attrs['tweet'])
+        #mentions to '@'
+        if mention:
+            tweet = re.sub(r'@\w+', '@', tweet)   
+
+        #remove newline characters
+        if newline:
+            tweet = tweet.replace('\n', '')       
+            pattern = f"[{re.escape(string.punctuation)}]"
+
+        #remove punctuation
+        if punctuation:
+            tweet = re.sub(pattern, '', tweet)
+
+        #remove stop words
+        if stop_words_:
+            tweet_words = tweet.split()
+            filtered_tweet = ' '.join([word for word in tweet_words if word.lower() not in stop_words]) 
+            attrs['tweet'] = filtered_tweet
+
     return dataset
