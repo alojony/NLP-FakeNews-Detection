d676d1335e4193505bb6403d104bd6dd954e6d9f
Xavier919 <xavier.lapointe@usherbrooke.ca>
2024-02-19 09:22:56 -0500

    up modules

--- modules/preprocess.py
+++ modules/preprocess.py
@@ -5,38 +5,52 @@ import nltk
 from nltk.corpus import stopwords
 
 nltk.download('stopwords')
+nlp = spacy.load("en_core_web_sm")  
+
 
 def spacy_tokenizer(text):
     nlp = spacy.load("en_core_web_sm")
     return [tok.text for tok in nlp.tokenizer(str(text))]
 
-def text_edit(dataset,num=False,mention=False,newline=False,punctuation=False,stop_words_=False):
+def text_edit(dataset,grp_num=False,grp_mention=False,rm_newline=False,rm_punctuation=False,rm_stop_words=False, lowercase=False, lemmatize=False):
     stop_words = set(stopwords.words('english')) #define set of english stop words
-    
+
     for attrs in dataset.values():
-        tweet = attrs['tweet']
+        if lowercase:
+            attrs['tweet'] = attrs['tweet'].lower()
 
+    for attrs in dataset.values():
+        if rm_stop_words:
+            tweet_words = attrs['tweet'].split()
+            filtered_tweet = ' '.join([word for word in tweet_words if word not in stop_words]) 
+            attrs['tweet'] = filtered_tweet
+
+    for attrs in dataset.values():
         #all numbers to 'num'
-        if num:
-            tweet = re.sub(r'\d+', 'num', tweet)  
+        if grp_num:
+            attrs['tweet'] = re.sub(r'\d+', 'num', attrs['tweet'])  
 
+    for attrs in dataset.values():
         #mentions to '@'
-        if mention:
-            tweet = re.sub(r'@\w+', '@', tweet)   
+        if grp_mention:
+            attrs['tweet'] = re.sub(r'@\w+', '@', attrs['tweet'])
 
+    for attrs in dataset.values():
         #remove newline characters
-        if newline:
-            tweet = tweet.replace('\n', '')       
-            pattern = f"[{re.escape(string.punctuation)}]"
+        if rm_newline:
+            attrs['tweet'] = attrs['tweet'].replace('\n', '')
 
+    for attrs in dataset.values():
         #remove punctuation
-        if punctuation:
-            tweet = re.sub(pattern, '', tweet)
+        pattern = f"[{re.escape(string.punctuation)}]"
+        if rm_punctuation:
+            attrs['tweet'] = re.sub(pattern, '', attrs['tweet'])
+            attrs['tweet'] = re.sub(r' +', ' ', attrs['tweet'])
 
-        #remove stop words
-        if stop_words_:
-            tweet_words = tweet.split()
-            filtered_tweet = ' '.join([word for word in tweet_words if word.lower() not in stop_words]) 
+    for attrs in dataset.values():
+        if lemmatize:
+            tweet_words = attrs['tweet'].split()
+            filtered_tweet = ' '.join([tok.lemma_ for tok in nlp(' '.join(tweet_words))]) 
             attrs['tweet'] = filtered_tweet
 
     return dataset
--- modules/utils.py
+++ modules/utils.py
@@ -1,7 +1,44 @@
 import pandas as pd
+from sklearn.linear_model import LogisticRegression
+from sklearn.model_selection import GridSearchCV
+from sklearn.linear_model import LogisticRegression
+from sklearn.svm import SVC
+from sklearn.metrics import accuracy_score, recall_score, f1_score, precision_score
 
 def build_dataset(path, num_class_samples=-1, rnd_state=42):
     df = pd.read_excel(path, engine='openpyxl')
+    #df = df[df['5_label_majority_answer'] == 'Agree']
     if num_class_samples != -1:
         df = df.sample(n=min(len(df), num_class_samples), replace=False, random_state=rnd_state)
     return df.T.to_dict()
+
+def tune_logistic_regression(X_train, Y_train):
+    model = LogisticRegression()
+    param_grid = {'solver': ['newton-cg', 'lbfgs', 'liblinear', 'saga'],
+                  'penalty': ['l2'],
+                  'C': [0.001, 0.01, 0.1, 1, 10, 100],
+                  'max_iter': [5000]}
+    grid_search = GridSearchCV(estimator=model, param_grid=param_grid, cv=5, scoring='f1_weighted', verbose=1, n_jobs=-1)
+    grid_search.fit(X_train, Y_train)
+    print(f"Best Hyperparameters: {grid_search.best_params_}")
+    best_model = grid_search.best_estimator_
+    return best_model
+
+def tune_svm(X_train, Y_train):
+    model = SVC()
+    param_grid = {'C': [0.001, 0.01, 0.1, 1, 10, 100],
+        'kernel': ['linear', 'poly', 'rbf', 'sigmoid'],  
+        'degree': [2, 3, 4, 5, 6, 7, 8, 9],
+        'gamma': ['scale', 'auto'], 
+        }
+    grid_search = GridSearchCV(estimator=model, param_grid=param_grid, cv=5, scoring='f1_weighted', verbose=1, n_jobs=-1)
+    grid_search.fit(X_train, Y_train)
+    print(f"Best Hyperparameters: {grid_search.best_params_}")
+    best_model = grid_search.best_estimator_
+    return best_model
+
+def evaluate(Y_test, y_pred):
+    print('Precision: ', precision_score(Y_test, y_pred))
+    print('Recall: ', recall_score(Y_test, y_pred))
+    print('F1_score: ', f1_score(Y_test, y_pred))
+    print('accuracy: ', accuracy_score(Y_test, y_pred))
